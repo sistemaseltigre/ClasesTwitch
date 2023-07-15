2dacc2b31787ffec412cfd493e59b62a2bbff828
Jesus Silva <sistemaseltigre@gmail.com>
2023-07-15 16:15:44 -0400

    create solana-pay example

---
+++ solana-pay/index.js
@@ -0,0 +1,81 @@
+//import solana web3
+const web3 = require("@solana/web3.js");
+const { Transaction, SystemProgram, Keypair, LAMPORTS_PER_SOL, Connection, PublicKey, sendAndConfirmTransaction, TransactionInstruction} = web3;
+
+//import solanapay
+const solpay = require("@solana/pay");
+const { encodeURL, validateTransfer, parseURL, TransferRequestURL, findReference} = solpay;
+
+//import bigNumber
+const BigNumberpkg = require("bignumber.js");
+const { BigNumber } = BigNumberpkg;
+
+//importamos wallet
+const fs = require('fs');
+const secret = JSON.parse(fs.readFileSync("frasesemilla.json").toString().trim());
+const userWallet = Keypair.fromSecretKey(new Uint8Array(secret));
+
+// CONSTANTS
+//wallet recipient's config
+const myWallet = 'GKzwank3rqV73sJPM1QWbmDN4sL9tcy3KTNhTnZDVaoT'; 
+const recipient = new PublicKey(myWallet);
+
+//config endpoint and cluster
+const quickNodeEndpoint = 'https://clean-blissful-patron.solana-devnet.quiknode.pro/0c1c6fd14118b66a3e7f4f5edc764738e801996d/';
+const connection = new Connection(quickNodeEndpoint, 'confirmed');
+
+//Product parameters
+const amount = new BigNumber(1.75); // 0.1 SOL
+const reference = new Keypair().publicKey;
+const label = 'Elchuo160 Swag';
+const message = `Elchuo160 - Order ID #EL-0${Math.floor(Math.random() * 999999) + 1}`;
+const memo = 'Elchuo160 Solana Pay Demo - Public Memo';
+
+
+
+async function generateUrl(recipient,amount,reference,label,message,memo) {
+    console.log('1. Create a payment request link');
+    const url = encodeURL({ recipient, amount, reference, label, message, memo });
+    console.log('Payment request link:', url);
+    return url;
+}
+
+(async () => {
+    try {
+        const url = await generateUrl(recipient, amount, reference, label, message, memo);
+        console.log('Success');
+    } catch (err) {
+        console.error(err);
+    }
+})();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ solana-pay/wallet.js
@@ -0,0 +1,40 @@
+const web3 = require("@solana/web3.js");
+const {Keypair, LAMPORTS_PER_SOL, Connection} = web3;
+const fs = require('fs');
+
+//STEP 1 - Connect to Solana Network
+const endpoint = 'https://clean-blissful-patron.solana-devnet.quiknode.pro/0c1c6fd14118b66a3e7f4f5edc764738e801996d/'; //Replace with your RPC Endpoint
+const solanaConnection = new Connection(endpoint);
+
+//STEP 2 - Generate a New Solana Wallet
+const keypair = Keypair.generate();
+console.log(`Generated new KeyPair. Wallet PublicKey: `, keypair.publicKey.toString());
+
+//STEP 3 - Write Wallet Secret Key to a .JSON
+const secret_array = keypair.secretKey    
+    .toString() //convert secret key to string
+    .split(',') //delimit string by commas and convert to an array of strings
+    .map(value=>Number(value)); //convert string values to numbers inside the array
+
+const secret = JSON.stringify(secret_array); //Covert to JSON string
+
+fs.writeFile('frasesemilla.json', secret, 'utf8', function(err) {
+    if (err) throw err;
+    console.log('Wrote secret key to frasesemilla.json.');
+    });
+
+//STEP 4 - Airdrop 1 SOL to new wallet
+(async()=>{
+    const airdropSignature = solanaConnection.requestAirdrop(
+        keypair.publicKey,
+        LAMPORTS_PER_SOL,
+    );
+    try{
+        const txId = await airdropSignature;     
+        console.log(`Airdrop Transaction Id: ${txId}`);        
+        console.log(`https://explorer.solana.com/tx/${txId}?cluster=devnet`)
+    }
+    catch(err){
+        console.log(err);
+    }    
+})()
